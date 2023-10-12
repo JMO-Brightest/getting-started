d050cdae1ccd1cb36e2c993b9751e50ec58a9dfe
Joren Molenaers <joren.molenaers@brightest.be>
2023-10-12 15:47:20 +0200

    add debug

--- app/src/index.js
+++ app/src/index.js
@@ -5,10 +5,12 @@ const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+const debug = require('./routes/debug');
 
 app.use(express.json());
 app.use(express.static(__dirname + '/static'));
 
+app.get('/debug',debug);
 app.get('/items', getItems);
 app.post('/items', addItem);
 app.put('/items/:id', updateItem);
